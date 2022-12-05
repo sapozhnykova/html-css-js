c15dc3eaf99a9ab96e56cad3ee773b016c817242
sapozhnykova <117081661+sapozhnykova@users.noreply.github.com>
2022-12-05 15:38:26 +0200

    Add files via upload

---
+++ js/task.js
@@ -0,0 +1,25 @@
+var x = "city";
+x = "Київ";
+x = "Львів";
+var y = "address";
+y = x;
+{
+  alert("address");
+}
+
+var x = 3;
+const result = 4 ** 3;
+console.log (result);
+
+var x = 5.25;
+var y = 3.50;
+var a = 4;
+var b = 6;
+var c = x * a;
+var d = y * b;
+var total = c + d;
+function newContent() {
+document.open();
+document.write(total);
+document.close;
+}
